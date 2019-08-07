27485a2e01407629f0c53f9c0a086e4678de21c4
cintogia <pietro@tallari.co>
2019-08-07 14:54:28 +0200

    style: added ARIA

--- js/main.js
+++ js/main.js
@@ -178,7 +178,7 @@ createRestaurantHTML = restaurant => {
   image.alt = `restaurant ${restaurant.name}`;
   li.append(image);
 
-  const name = document.createElement("h1");
+  const name = document.createElement("h2");
   name.innerHTML = restaurant.name;
   li.append(name);
 
